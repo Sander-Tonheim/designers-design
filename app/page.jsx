69eab08f94ce4237b9f408b1daad86af7e3b9107
Sander Tonheim <tonheims98@gmail.com>
2024-11-22 14:05:50 +0100

    updated imported font

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { Michroma } from "next/font/google";
+import { Doto } from "next/font/google";
 import LottieAnimationFrame from "@/components/lottieAnimationFrame";
 import { animations } from "@/public/lottie/index";
 import abstactShapes from "@/public/images/abstractShapes.jpg";
 import designSectionBackground from "@/public/images/designSectionBackground.jpg";
 
-const doto = Michroma({
+const doto = Doto({
   subsets: ["latin"],
   display: "swap",
   weight: "400",
