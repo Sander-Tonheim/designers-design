b993053dfb575068fb8250906befc68160ecac7c
Sander Tonheim <tonheims98@gmail.com>
2024-11-22 14:06:45 +0100

    adjusted margin value

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ const doto = Doto({
 export default function Home() {
   return (
     <>
-      <div className="HERO flex h-[43rem] flex-col items-center gap-20 px-8 py-10 md:flex-row md:justify-between lg:mb-32 lg:h-[43rem]">
+      <div className="HERO flex h-[43rem] flex-col items-center gap-20 px-8 py-10 md:flex-row md:justify-between lg:mb-28 lg:h-[43rem]">
         <div
           className={`HEADER flex flex-col text-4xl xs:text-5xl sm:text-6xl md:text-6xl lg:text-7xl ${doto.className}`}
         >
